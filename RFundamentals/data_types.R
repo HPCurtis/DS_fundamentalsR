fd80b3572a43c84d95830b126228d34824cc303e
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-08-02 16:25:08 +0100

    data_types script

---
+++ RFundamentals/data_types.R
@@ -0,0 +1,15 @@
+earnings <- 200000L
+tax_rate <- 0.30
+tax_owed <- earnings * tax_rate
+
+print(typeof(earnings))
+print(typeof(tax_rate))
+print(typeof(tax_owed))
+
+# Character type
+print(typeof("ABC"))
+print(typeof('A'))
+
+# Logical
+print(typeof(5 > 4))
+
