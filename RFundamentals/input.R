2ee0faf599804b29c41e5c664d60132ebcc65612
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-08-04 10:36:45 +0100

    user input scripts

---
+++ RFundamentals/input.R
@@ -0,0 +1,6 @@
+location <- readline(prompt="where do you live? ")
+
+print(paste0("Very nice, I hear" , location, " is buzzing at this time of year."))
+
+#convert input strings as needed
+fav_num = as.integer(readline(prompt="What is your favourite number? "))
