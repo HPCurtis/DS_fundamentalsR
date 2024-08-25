60602e88044f8445e63636bfb0bc619e6fc31022
Harrison <harrisoncurtis50@gmail.com>
2024-08-25 16:45:14 +0100

    Adding matrix operations

--- RFundamentals/vector_operations.R
+++ RFundamentals/vector_operations.R
@@ -9,3 +9,17 @@ zeros[1] = 50
 # meets an certain value using R's which
 # function
 print(which(zeros == 50))
+
+# Generate 2x3 matrix in R.
+m <- matrix(c(1,5,9,8,5,5), nrow = 2, byrow=TRUE)
+
+# Get the row/column inidcies of the value specified.
+print(which(m == 5, arr.ind=TRUE))
+
+
+# Column-major order (linear indexing):
+# Index:  1   3   5
+#         2   4   6
+# Values: 1   5   9
+#         8   5   5
+print(which(m == 5))
